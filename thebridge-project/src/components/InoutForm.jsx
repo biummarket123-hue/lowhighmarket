c55f0909b2f46dc2e45d8299ae29245bd6ded8d5
biummarket123-hue <biummarket123@gmail.com>
2026-04-06 10:28:31 +0900

    Update InoutForm.jsx

--- thebridge-project/src/components/InoutForm.jsx
+++ thebridge-project/src/components/InoutForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Tag, SecTitle, Card, Empty, Toast, PrimaryBtn, GhostBtn, FLabel, ConfirmModal, EditOrderModal, EditInvModal, EditCustModal, ShippingModal } from "./components/UI.jsx";
+import { Tag, SecTitle, Card, Empty, Toast, PrimaryBtn, GhostBtn, FLabel, ConfirmModal, EditOrderModal, EditInvModal, EditCustModal, ShippingModal } from "./UI.jsx";
 import { G, SF, S, REWARDS_DATA, INIT_DATA, DEFAULT_SETTINGS, DEFAULT_MANAGERS, DEFAULT_INV, STORAGE_KEYS } from "./constants.js";
 
 function InoutForm({inv, setInv, logs, setLogs, showToast}) {
